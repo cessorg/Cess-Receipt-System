66a41592bafbf95f0808654c1b60d1a7192d8af2
BHARAT <nischalbharat4819@gmail.com>
2019-08-28 19:40:52 +0530

    email credentials

--- mail.js
+++ mail.js
@@ -5,12 +5,12 @@ var transporter = nodemailer.createTransport({
     secure: true,
     auth: {
         type: 'OAuth2',
-        user: "manjotsingh16july@gmail.com",
-        clientId: "825656477336-78d1p99vljedfpm2bv181ntd1b1obta7.apps.googleusercontent.com",
-        clientSecret: "gRedgJh5F_nZjI0i_j6BgR84",
-        refreshToken: "1/ajJoXfqaj-3xkDxJzJDS2mdzDoO5vx4x4ax_OywuYY9TPqSPPBYcA1LaaVQ5P-ch",
-        accessToken: "ya29.GlvNBvjBJsFkOlstDauW45RYYEPEw270ZpwLhyWFlDEFEqwFNgPx9GPoWjmPuEOsrOCbXuXwon6y-rDD2cjIK7uWw9LhHaaOpTYrxJ4w11ATrSeEj-R4JK0LJRkh",
-        expires: 1484314697598
+        user: "email",
+        clientId: "id",
+        clientSecret: "secret key",
+        refreshToken: "token",
+        accessToken: "token",
+        expires: 1234
     }
 });
 
